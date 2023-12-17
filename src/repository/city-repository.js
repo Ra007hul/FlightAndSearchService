7aa62590282483f27bb7cef3df44d95607169dcb
unknown <007rahul9254@gmail.com>
2023-12-17 16:03:04 +0530

    Completed city Repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -2,27 +2,57 @@ const { City }= require("../models/index.js");
 
 class CityRepository{
 
-    async createCity({ name }){
-        try{
-        const city = await City.create({ name });
-        return city; 
-         }
-        catch(error){
+   async createCity({ name }){
+        try {
+            const city= await City.create({
+                name:name    
+            });
+
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
-         }
-    }
-    async deleteCity(cityId){
-        try{
-             await City.destroy({
-                where : {
+        }
+   }
+
+   async deleteCity(cityId){
+    try{
+           await City.destroy({
+            where : {
+                id: cityId
+            }
+           })
+           return true;
+        }
+
+        catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+   }
+   async updateCity(cityId,data){
+         try {
+            const city = await City.update(data,{
+                where:{
                     id : cityId
                 }
-              });
-        }
-        catch(error){
-                throw {error};
-        }
-    }
+            })
+         } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+         }
+   }
+   async getCity(cityId){
+    try{
+        const city = await City.findByPk(cityId);
+        return city;
+     }
+
+     catch(error){
+         console.log("Something went wrong in the repository layer");
+         throw {error};
+     }
+   }
 }
 
 module.exports = CityRepository;
