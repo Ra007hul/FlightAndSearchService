11a64b1363fa96b84d5092426a72fdb72796df96
unknown <007rahul9254@gmail.com>
2023-12-20 11:55:32 +0530

    Added airports model

--- src/models/airport.js
+++ src/models/airport.js
@@ -1,9 +1,9 @@
 'use strict';
 const {
-  Model, UniqueConstraintError
+  Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class City extends Model {
+  class Airport extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,22 +11,26 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-        this.hasMany(models.Airport , {
-          foreignKey: 'cityId'
-        })
-
+      this.belongsTo(models.city , {
+        foreignKey : 'cityId',
+        onDelete : 'CASCADE'
+      })
     }
   }
-  City.init({
+  Airport.init({
     name: {
-      type : DataTypes.STRING,
-      allowNull : false,
-      unique:true
+       type : DataTypes.STRING,
+      allowNull:false
+    },
+    address:{type: DataTypes.STRING
+      
+    },
+    cityId:{type: DataTypes.INTEGER ,
+      allowNull : false
     }
-    
   }, {
     sequelize,
-    modelName: 'City',
+    modelName: 'Airport',
   });
-  return City;
+  return Airport;
 };
